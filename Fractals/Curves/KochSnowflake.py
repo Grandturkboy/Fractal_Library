a1d43016e1ef705214002ebdf5c776aa9b071f86
Regoczi Aron <regocziaron@gmail.com>
2025-10-29 19:30:38 +0100

    Implement rainbow color option for Koch curve
    Added rainbow toggle feature for Koch curve drawing.

--- Fractals/Curves/KochSnowflake.py
+++ Fractals/Curves/KochSnowflake.py
@@ -25,8 +25,19 @@ size_slider.set(400)
 size_slider.pack()
 
 angle_slider = tk.Scale(root, from_=0, to=180, resolution=3, orient="horizontal", label="Angle")
-angle_slider.set(45)    
+angle_slider.set(30)    
 angle_slider.pack()
+
+def toggleRainbow():
+    global rainbow
+    rainbow = not rainbow
+    redraw()
+
+rainbow = False
+rainbowButton = tk.Checkbutton(root, text="Rainbow", command=toggleRainbow, width=20)
+rainbowButton.pack()
+colors = ["#FF0000","#FF1900","#FF3300","#FF4C00","#FF6600","#FF7F00","#FF9900","#FFB200","#FFCC00","#FFE500","#FFFF00","#E5FF00","#CCFF00","#B2FF00","#99FF00","#7FFF00","#66FF00","#4CFF00","#33FF00","#19FF00","#00FF00","#00FF19","#00FF33","#00FF4C","#00FF66","#00FF7F","#00FF99","#00FFB2","#00FFCC","#00FFE5","#00FFFF","#00E5FF","#00CCFF","#00B2FF","#0099FF","#007FFF","#0066FF","#004CFF","#0033FF","#0019FF","#0000FF","#1900FF","#3300FF","#4C00FF","#6600FF","#7F00FF","#9900FF","#B200FF","#CC00FF","#E500FF","#FF00FF"]
+
 
 previousIter = iteration_slider.get()
 previousSize = size_slider.get()
@@ -43,6 +54,13 @@ def KochIt(iter, angle, length):
     if iter == 0:
         t.forward(length)
         return
+    
+    if rainbow:
+        t.pencolor(colors[functionsUsed % len(colors)])
+        t.pensize(3)
+    else:
+        t.pencolor("black")
+        t.pensize(1)
 
     functionsUsed += 1
     functionCounter.config(text=f"Functions used: {functionsUsed}")
